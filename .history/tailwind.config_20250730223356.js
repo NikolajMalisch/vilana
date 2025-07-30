387b3d141cd630c1779d1793be361f6c4b2ceb71
Malisch <NikolajMalisch>
2025-07-30 23:06:06 +0200

    mod menu.html

--- .history/tailwind.config_20250730223356.js
+++ .history/tailwind.config_20250730223356.js
@@ -2,12 +2,20 @@
 module.exports = {
 content: ["./*.html"],
 theme: {
-    extend: {
-    fontFamily: {
-        cinzel: ['"Cinzel Decorative"', 'cursive'],
-        cormorant: ['"Cormorant SC"', 'serif'],
-    },
-    },
+    extend: {},
 },
 plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: ["./*.html"],
+theme: {
+    extend: {
+      fontFamily: {
+        cinzel: ['"Cinzel Decorative"', 'cursive'],
+        cormorant: ['"Cormorant SC"', 'serif'],
+      },
+    },
+  },
+  plugins: [],
 }
--- .history/tailwind.config_20250730223414.js
+++ .history/tailwind.config_20250730223414.js
@@ -2,6 +2,14 @@
 module.exports = {
 content: ["./*.html"],
 theme: {
+    extend: {},
+},
+plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: ["./*.html"],
+theme: {
     extend: {
     fontFamily: {
         cinzel: ['"Cinzel Decorative"', 'cursive'],
