e4e24e5662e573b24b73aa9456604764a466b0a4
Malisch <NikolajMalisch>
2025-08-20 15:04:23 +0200

    fix: добавить поддержку темной и светлой схемы в мета-тегах в bewertung.html; оптимизировать конфигурацию tailwind в tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-content: ["./*.html", "./**/*.html", "./js/**/*.js"],
-theme: {
-extend:{
-colors: {
-brand: { 600: "#c9b192" } // для border-brand-600
-    }
-    }
-},
-plugins: []
-};
+content: ["./*.html","./**/*.html","./js/**/*.js"],
+theme: { extend: { fontFamily:{
+    cinzel:["'Cinzel Decorative'","cursive"],
+    cormorant:["'Cormorant SC'","serif"],
+}}},
+plugins:[]
+}
